8f11ff3bdc76a8eb391d1c41cb75f6a101d295df
PJ Walton <philip.walton@ontariotechu.net>
2020-10-27 18:04:16 -0400

    got compass working

--- Major_Project/lib/live chat/live_chat.dart
+++ Major_Project/lib/live chat/live_chat.dart
@@ -1,9 +1,65 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'dart:math' as math;
 
-class LiveChat extends StatelessWidget {
+class LiveChat extends StatefulWidget {
   LiveChat({Key key}) : super(key: key);
 
-  Widget build(BuildContext context) {
+  _LiveChatState createState() => _LiveChatState();
+}
 
+class _LiveChatState extends State<LiveChat> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(title: const Text("ABC")),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: <Widget>[
+              StreamBuilder(
+                stream: FlutterCompass.events,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Text('Error reading heading: ${snapshot.error}');
+                  }
+
+                  DateTime now = DateTime.now();
+                  double direction = snapshot.data;
+
+                  return Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Text(
+                          '$direction',
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .caption,
+                        ),
+                        Text(
+                          '$now',
+                          style: Theme
+                              .of(context)
+                              .textTheme
+                              .caption,
+                        ),
+                      ],
+                    ),
+                  );
+                }
+              )
+            ],
+          ),
+        )
+    );
   }
 }
--- Major_Project/lib/live chat/live_chat_page.dart
+++ Major_Project/lib/live chat/live_chat_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_compass/flutter_compass.dart';
-import 'dart:math' as math;
+import 'package:major_project/live%20chat/live_chat.dart';
 
 class LiveChatPage extends StatefulWidget {
   LiveChatPage({Key key}) : super(key: key);
@@ -9,8 +8,6 @@ class LiveChatPage extends StatefulWidget {
 }
 
 class _LiveChatPageState extends State<LiveChatPage> {
-  double _lastRead = 0;
-  DateTime _lastReadAt;
 
   @override
   void initState() {
@@ -19,91 +16,6 @@ class _LiveChatPageState extends State<LiveChatPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: const Text('Flutter Compass'),
-        ),
-        body: Builder(builder: (context) {
-
-          return Column(
-            children: <Widget>[
-              _buildManualReader(),
-              Expanded(child: _buildCompass()),
-            ],
-          );
-
-        }),
-      ),
-    );
-  }
-
-  Widget _buildManualReader() {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        children: <Widget>[
-          RaisedButton(
-            child: Text('Read Value'),
-            onPressed: () async {
-              final double tmp = await FlutterCompass.events.first;
-              setState(() {
-                _lastRead = tmp;
-                _lastReadAt = DateTime.now();
-              });
-            },
-          ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: <Widget>[
-                Text(
-                  '$_lastRead',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-                Text(
-                  '$_lastReadAt',
-                  style: Theme.of(context).textTheme.caption,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildCompass() {
-    return StreamBuilder<double>(
-      stream: FlutterCompass.events,
-      builder: (context, snapshot) {
-        if (snapshot.hasError) {
-          return Text('Error reading heading: ${snapshot.error}');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        double direction = snapshot.data;
-
-        // if direction is null, then device does not support this sensor
-        // show error message
-        if (direction == null)
-          return Center(
-            child: Text("Device does not have sensors !"),
-          );
-
-        return Container(
-          alignment: Alignment.center,
-          child: Transform.rotate(
-            angle: ((direction ?? 0) * (math.pi / 180) * -1),
-          ),
-        );
-      },
-    );
+    return LiveChat();
   }
 }
